9a907e12c2d4e017b2c55ff2736cb1b22650c758
Mathis <mathis.branet@gmail.com>
2023-10-17 11:53:51 +0200

    mise en place de la fenêtre et de sa fermeture

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("ok")
+import pygame
+
+pygame.init()
+
+def main():
+
+    screen = pygame.display.set_mode((800, 720))
+    pygame.display.set_caption("Tetris")
+
+    run = True
+
+    while run:
+        pygame.display.flip()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_ESCAPE]:
+            run = False
+
+if __name__ == '__main__':
+    main()

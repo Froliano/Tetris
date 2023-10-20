c81a64c9b23d56f0d1eb02656989fc2958436ec6
Mathis <mathis.branet@gmail.com>
2023-10-20 13:44:59 +0200

    ajout des collisions

--- block.py
+++ block.py
@@ -9,15 +9,40 @@ class Block(pygame.sprite.Sprite):
         self.color = color
         self.bg = (130, 130, 130)
 
-        self.size = 32
-        self.offset = 2
+        self.size = 64
+        self.offset = 4
 
     def draw(self, screen):
         pygame.draw.rect(screen, self.bg, pygame.Rect(self.x, self.y, self.size, self.size))
         pygame.draw.rect(screen, self.color, pygame.Rect(self.x+self.offset, self.y+self.offset, self.size-self.offset*2, self.size-self.offset*2))
+
+    def collide(self, block):
+        #verifier si le coin en bas à droite du block est dans le block self
+        if self.x >= block.x and self.x <= block.x + block.size and self.y >= block.y and self.y <= block.y + block.size:
+            return True
+
+        # verifier si le coin en haut à droite du block est dans le block self
+        if self.x >= block.x and self.x <= block.x + block.size and self.y + self.size >= block.y and self.y + self.size <= block.y + block.size:
+            return True
+
+        # verifier si le coin en bas à gauche du block est dans le block self
+        if self.x + self.size >= block.x and self.x + self.size <= block.x + block.size and self.y >= block.y and self.y <= block.y + block.size:
+            return True
+
+        # verifier si le coin en haut à gauche du block est dans le block self
+        if self.x + self.size >= block.x and self.x + self.size <= block.x + block.size and self.y + self.size >= block.y and self.y + self.size <= block.y + block.size:
+            return True
+
+        return False
 
     def left(self):
         self.x -= 1
 
     def right(self):
         self.x += 1
+
+    def top(self):
+        self.y -= 1
+
+    def down(self):
+        self.y += 1
--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 import pygame
 from block import Block
 
+
 pygame.init()
 
 def main():
 
     screen = pygame.display.set_mode((800, 720))
     pygame.display.set_caption("Tetris")
-    group = pygame.sprite.Group()
+    clock = pygame.time.Clock()
+
     run = True
-    block = Block(10, 10, (255, 255, 255))
-    block2 = Block(500, 10, (255, 0, 0))
+
+    block = Block(100, 60, (255, 255, 255))
+    block2 = Block(10, 10, (255, 0, 0))
+
+
+    group = pygame.sprite.Group()
     group.add(block)
     group.add(block2)
 
@@ -18,6 +24,8 @@ def main():
         screen.fill((0, 0, 0))
         block.draw(screen)
         block2.draw(screen)
+        if not block.collide(block2):
+            print("ok")
 
 
         for event in pygame.event.get():
@@ -27,12 +35,17 @@ def main():
         if pressed[pygame.K_ESCAPE]:
             run = False
         if pressed[pygame.K_LEFT]:
-            block.left()
+            block2.left()
         if pressed[pygame.K_RIGHT]:
-            block.right()
+            block2.right()
+        if pressed[pygame.K_UP]:
+            block2.top()
+        if pressed[pygame.K_DOWN]:
+            block2.down()
 
 
         pygame.display.flip()
+        clock.tick(100)
 
 if __name__ == '__main__':
     main()
